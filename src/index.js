1fc1f66c02b8f5f50fda7e90fc3553f09c6d80ce
Prajwol Devkota <prajwol.devkota016@gmail.com>
2021-12-23 06:40:56 +0100

    Clone amazon book.com

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// vars
+const firstBook = {
+  author: 'Colleen Hoover',
+  title: 'It Ends with Us: A Novel',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71EwoNQypZL._AC_UL604_SR604,400_.jpg'
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  author: 'Hopscotch Girls',
+  title: 'I Am Confident, Brave & Beautiful: A Coloring Book for Girls',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/71AIzioFxTL._AC_UL604_SR604,400_.jpg'
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  return (
+    <article className="book">
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
